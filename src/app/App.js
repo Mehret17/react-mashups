8012a2484facb47be3ff1ef4eab276551c9e1248
Mehret Kulala <merutaye@gmail.com>
2018-07-02 17:41:38 -0500

    firebase config works and get function for animal works

--- src/app/App.js
+++ src/app/App.js
@@ -1,16 +1,34 @@
 import React, { Component } from 'react';
 
+import connection from '../firebaseRequests/connection';
+import animalRequest from '../firebaseRequests/animals';
 import Animals from '../components/Animals/Animals';
+
 
 
 import './App.css';
 import MashupForm from '../components/MashupForm/MashupForm';
 
 class App extends Component {
+  state = {
+    animals: [],
+  }
+  componentDidMount () {
+    connection();
+    animalRequest.getRequest()
+    .then((animals) => {
+      this.setState ({ animals });
+    })
+    .catch((err) => {
+      console.error('error with mashup GET', err);
+    });
+  };
+
   render() {
     return (
       <div>
-        <Animals />
+        <Animals 
+         animals={this.state.animals}/>
         <MashupForm />
       </div>
     );
---
+++ src/firebaseRequests/animals.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+import constants from '../constants';
+
+const getRequest = () => {
+  return new Promise ((resolve, reject) => {
+    axios
+      .get(`${constants.firebaseConfig.databaseURL}/mashups.json`)
+      .then((res) => {
+        const mashups = [];
+        if (res.data !==null) {
+          Object.keys(res.data).forEach((key) => {
+            res.data[key].id = key;
+            mashups.push(res.data[key]);
+          });
+        }
+        console.error('err', res);
+        resolve(mashups);
+      })
+       .catch((err) => {
+         reject(err);
+       });
+  });
+};
+
+export default {getRequest};
